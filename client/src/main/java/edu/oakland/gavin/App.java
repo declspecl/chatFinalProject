1b134ac4caf0de9a3ad8e354637dd6762c0e766f
dec-lied <gavindhondt@hotmail.com>
2024-04-01 11:47:06 -0400

    mostly done with new QoL room number process

--- client/src/main/java/edu/oakland/gavin/App.java
+++ client/src/main/java/edu/oakland/gavin/App.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -11,27 +12,105 @@ public class App
 {
     public static void main(String[] args)
     {
-        try (Socket socket = new Socket())
+        // try-with-resource syntax to automatically close the server socket
+        try (Socket server = new Socket())
         {
-            // spawn socket connection to localhost on port 9999
-            socket.connect(new InetSocketAddress("127.0.0.1", 9999));
-
+            // spawn socket connection to server on port 9999
+            server.connect(new InetSocketAddress("127.0.0.1", 9999));
             Logger.success("Connected to server");
 
-            // open output channel to server
-            OutputStream rawOutputStream = socket.getOutputStream();
-            DataOutputStream dataOutputStream = new DataOutputStream(rawOutputStream);
-            OutputStreamWriter charOutputStream = new OutputStreamWriter(rawOutputStream);
+            // attempt to get data input stream from server, quitting if unsuccessful
+            final DataInputStream dataInputStream;
+            try
+            {
+                dataInputStream = new DataInputStream(server.getInputStream());
+            }
+            catch (IOException e)
+            {
+                Logger.error(String.format("Failed to get data input stream from server: %s", e));
+                return;
+            }
 
-            InputStreamReader charInputStream = new InputStreamReader(socket.getInputStream());
+            // attempt to get data input stream from server, quitting if unsuccessful
+            final DataOutputStream dataOutputStream;
+            try
+            {
+                dataOutputStream = new DataOutputStream(server.getOutputStream());
+            }
+            catch (IOException e)
+            {
+                Logger.error(String.format("Failed to get data output stream from serveer: %s", e));
+                return;
+            }
 
-            // reading the room number from the user
+            ArrayList<Integer> activeRoomNumbers = new ArrayList<>();
+
+            // keep reading room numbers from the server until we get a -1 (meaning the server is done sending them)
+            int recentRoomNumber = 0;
+            while (recentRoomNumber != -1)
+            {
+                try
+                {
+                    recentRoomNumber = dataInputStream.readInt();
+
+                    if (recentRoomNumber != -1)
+                    {
+                        activeRoomNumbers.add(recentRoomNumber);
+                    }
+                }
+                catch (IOException e)
+                {
+                    Logger.error("Failed to read room number from server");
+                    return;
+                }
+            }
+
+            // print out the active room numbers to the user, if there are any
+            if (activeRoomNumbers.isEmpty())
+            {
+                Logger.info("There are currently no rooms with any active users");
+            }
+            else
+            {
+                Logger.info("There are active users in the following rooms:");
+
+                // iterate through each active room and print it
+                for (int roomNumber : activeRoomNumbers)
+                {
+                    System.out.printf("- %d\n", roomNumber);
+                }
+            }
+
+            // reading the room number they want to join from the user
             Scanner scanner = new Scanner(System.in);
 
-            Logger.info("Enter room number to join:");
+            // keep asking the user which room to join until they give a valid room number
+            int targetRoom = -1;
+            while (targetRoom == -1)
+            {
+                System.out.print("Enter any room number to join: ");
 
-            int targetRoom = Integer.parseInt(scanner.nextLine());
-            dataOutputStream.writeInt(targetRoom);
+                try
+                {
+                    targetRoom = Integer.parseInt(scanner.nextLine());
+                }
+                catch (Exception e)
+                {
+                    Logger.error("Please input a valid room number");
+                    continue;
+                }
+            }
+
+            // try sending the room number to the server
+            try
+            {
+                dataOutputStream.writeInt(targetRoom);
+            }
+            catch (IOException e)
+            {
+                Logger.error(String.format("Failed to send the room number to server: %d", targetRoom));
+                return;
+            }
 
             Logger.success(String.format("Joined room %d\n", targetRoom));
 
@@ -39,11 +118,11 @@ public class App
             new Thread(() -> {
                 try
                 {
-                    while (!socket.isClosed())
+                    while (!server.isClosed())
                     {
                         char[] characters = new char[1000];
 
-                        // read characters from the socket
+                        // read characters from the server
                         int bytesRead = charInputStream.read(characters);
 
                         // if nothing was read, try again
@@ -52,7 +131,7 @@ public class App
                         // if -1 was read, the stream ended
                         if (bytesRead == -1)
                         {
-                            Logger.warning(String.format("Connection with %s was unexpectedly closed", socket));
+                            Logger.warning(String.format("Connection with %s was unexpectedly closed", server));
                             break;
                         }
 
@@ -63,12 +142,12 @@ public class App
                 }
                 catch (Exception e)
                 {
-                    Logger.error(String.format("Error occurred in handling socket: %s", e));
+                    Logger.error(String.format("Error occurred in handling server: %s", e));
                 }
             }).start();
 
             // getting input from user until the connection with the server closes
-            while (!socket.isClosed())
+            while (!server.isClosed())
             {
                 Logger.info("Enter message to send. Or type \"quit\" to leave the room.");
                 String message = scanner.nextLine();
@@ -84,19 +163,21 @@ public class App
 
             Logger.info("Disconnected from server");
 
-            // closing all streams and buffers
-            dataOutputStream.close();
-            charOutputStream.close();
-            rawOutputStream.close();
-            charInputStream.close();
-        }
-        catch (NumberFormatException e)
-        {
-            Logger.error("Invalid room number input supplied");
+            // closing all streams, connections, and buffers
+            try
+            {
+                dataInputStream.close();
+                dataOutputStream.close();
+                server.close();
+            }
+            catch (IOException e)
+            {
+                Logger.error("Failed to close one or more streams, connections, and/or buffers");
+            }
         }
         catch (IOException e)
         {
-            Logger.error(e.toString());
+            Logger.error("Failed to connect to server");
         }
     }
 }
--- server/src/main/java/edu/oakland/gavin/MultiThreadedServer.java
+++ server/src/main/java/edu/oakland/gavin/MultiThreadedServer.java
@@ -5,6 +5,9 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.net.ServerSocket;
+import java.util.Random;
+import java.util.Set;
+import java.util.random.RandomGenerator;
 
 public class MultiThreadedServer implements AutoCloseable
 {
@@ -55,6 +58,12 @@ public class MultiThreadedServer implements AutoCloseable
         {
             // if the room exists, remote the client from the set of connections
             this.roomToConnectionsMap.get(room).remove(client);
+
+            // after the last client in the room leaves, delete the room
+            if (this.roomToConnectionsMap.get(room).isEmpty())
+            {
+                this.roomToConnectionsMap.remove(room);
+            }
         }
         else
         {
@@ -68,80 +77,125 @@ public class MultiThreadedServer implements AutoCloseable
         // spawn new thread to manage new client's connection
         new Thread(() ->
         {
+            // attempt to get data input stream from client, quitting if unsuccessful
+            final DataInputStream dataInputStream;
             try
             {
-                InputStream rawInputStream = client.getInputStream();
-                DataInputStream dataInputStream = new DataInputStream(rawInputStream);
-
-                // read the room number the client asked to join
-                int clientTargetRoom = dataInputStream.readInt();
-                this.addClientToRoom(clientTargetRoom, client);
-                Logger.success(String.format("Client joined room %d", clientTargetRoom));
-
-                // open input stream with client
-                InputStreamReader charInputStream = new InputStreamReader(rawInputStream);
-
-                // keep reading messages from client until the connection closes
-                while (!client.isClosed())
-                {
-                    char[] characters = new char[1000];
-
-                    // read characters from the client
-                    int bytesRead = charInputStream.read(characters);
-
-                    // if nothing was read, try again
-                    if (bytesRead == 0) continue;
-
-                    // if -1 was read, the stream ended
-                    if (bytesRead == -1)
-                    {
-                        Logger.warning(String.format("Connection with %s was unexpectedly closed", client));
-                        break;
-                    }
-
-                    // if execution has gotten here, it means a message was received
-                    String message = new String(characters);
-                    Logger.success(String.format("Message received from %s in room %d: %s", client, clientTargetRoom, message));
-
-                    // send the received message to every other client in the same room
-                    for (Socket connection : this.roomToConnectionsMap.get(clientTargetRoom))
-                    {
-                        // we don't want to send the message to the client who sent it, so skip it if its the case
-                        if (client == connection) continue;
-
-                        // otherwise, send the message to the client
-                        OutputStreamWriter charOutputStream = new OutputStreamWriter(connection.getOutputStream());
-                        charOutputStream.write(message);
-                        charOutputStream.flush();
-                    }
-                }
-
-                // once the connection with the client has closed, remove the client from the room
-                this.removeClientFromRoom(clientTargetRoom, client);
-
-                Logger.info(String.format("Client %s in room %d disconnected", client, clientTargetRoom));
-
-                // close all streams and buffers to client
-                dataInputStream.close();
-                charInputStream.close();
-                rawInputStream.close();
-
+                dataInputStream = new DataInputStream(client.getInputStream());
             }
             catch (IOException e)
             {
-                Logger.error(String.format("IOException for client %s: %s", client, e));
+                Logger.error(String.format("Failed to get data input stream from client: %s", e));
+                return;
             }
-            finally
+
+            // attempt to get data input stream from client, quitting if unsuccessful
+            final DataOutputStream dataOutputStream;
+            try
             {
+                dataOutputStream = new DataOutputStream(client.getOutputStream());
+            }
+            catch (IOException e)
+            {
+                Logger.error(String.format("Failed to get data output stream from client: %s", e));
+                return;
+            }
+
+            // getting all the room numbers that have one or more clients connected
+            final Set<Integer> activeRoomNumbers = this.roomToConnectionsMap.keySet();
+
+            System.out.println(activeRoomNumbers);
+
+            // send each of these room numbers to the client
+            for (int roomNumber : activeRoomNumbers)
+            {
+                System.out.printf("%d\n", roomNumber);
                 try
                 {
-                    // close the connection with the client at the end
-                    client.close();
+                    dataOutputStream.writeInt(roomNumber);
                 }
                 catch (IOException e)
                 {
-                    Logger.error(String.format("Failed to close socket %s", e));
+                    Logger.error(String.format("Failed to send room number %d to client", roomNumber));
+                    return;
                 }
+            }
+
+            // send -1 to the client so that the client knows when we are done sending room numbers
+            try
+            {
+                dataOutputStream.writeInt(-1);
+            }
+            catch (IOException e)
+            {
+                Logger.error("Failed to send finishing number -1 to client");
+                return;
+            }
+
+            int clientTargetRoomNumber;
+            try
+            {
+                clientTargetRoomNumber = dataInputStream.readInt();
+            }
+            catch (IOException e)
+            {
+                Logger.error("Failed to read client's target room number");
+                return;
+            }
+
+            // add client to their inputted room number
+            this.addClientToRoom(clientTargetRoomNumber, client);
+            Logger.success(String.format("Added client %s to room %d", client, clientTargetRoomNumber));
+
+            // keep reading messages from client until the connection closes
+            while (!client.isClosed())
+            {
+                String message;
+                try
+                {
+                    message = dataInputStream.readUTF();
+                }
+                catch (Exception e)
+                {
+                    Logger.error(String.format("Failed to read message from client %s", client));
+                    continue;
+                }
+
+                Logger.success(String.format("Message received from %s in room %d: %s", client, clientTargetRoomNumber, message));
+
+                // send the received message to every other client in the same room
+                for (Socket connection : this.roomToConnectionsMap.get(clientTargetRoomNumber))
+                {
+                    // we don't want to send the message to the client who sent it, so skip it if its the case
+                    if (client == connection) continue;
+
+                    // try to send message to all other clients
+                    try
+                    {
+                        DataOutputStream connectionDataOutputStream = new DataOutputStream(connection.getOutputStream());
+                        connectionDataOutputStream.writeUTF(message);
+                    }
+                    catch (IOException e)
+                    {
+                        Logger.error(String.format("Failed to send message to connection %s", connection));
+                    }
+                }
+            }
+
+            // once the connection with the client has closed, remove the client from the room
+            this.removeClientFromRoom(clientTargetRoomNumber, client);
+            Logger.info(String.format("Client %s in room %d disconnected", client, clientTargetRoomNumber));
+
+            // close all streams, connections, and buffers to client
+            try
+            {
+                dataInputStream.close();
+                dataOutputStream.close();
+                client.close();
+            }
+            catch (IOException e)
+            {
+                Logger.error("Failed to close one or more streams and connections to the client");
             }
         }).start();
     }
@@ -150,9 +204,9 @@ public class MultiThreadedServer implements AutoCloseable
     public void close() throws IOException
     {
         // close all client connections
-        for (HashSet<Socket> connections : this.roomToConnectionsMap.values())
+        for (HashSet<Socket> roomConnections : this.roomToConnectionsMap.values())
         {
-            for (Socket connection : connections)
+            for (Socket connection : roomConnections)
             {
                 connection.close();
             }
